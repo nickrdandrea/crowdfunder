361ff1baa4ef09b915c9954f5fc9e285db35cca6
tasconajoey <joey.tascona@gmail.com>
2017-02-22 15:25:29 -0500

    Add more associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+  has_many :pledges
+  has_many :projects
 
   validates :password, length: { minimum: 8 }, on: :create
   validates :password, confirmation: true, on: :create
